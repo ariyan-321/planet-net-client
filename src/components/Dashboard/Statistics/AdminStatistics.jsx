3f42cb1bdfd632961cb391f7c99e8c8b3d5be3fe
Your Name <you@example.com>
2025-01-12 11:33:27 +0600

    adding admin data

--- src/components/Dashboard/Statistics/AdminStatistics.jsx
+++ src/components/Dashboard/Statistics/AdminStatistics.jsx
@@ -1,8 +1,27 @@
 import { Calendar } from "react-date-range";
 import { FaUserAlt, FaDollarSign } from "react-icons/fa";
 import { BsFillCartPlusFill, BsFillHouseDoorFill } from "react-icons/bs";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import LoadingSpinner from "../../Shared/LoadingSpinner";
 
 const AdminStatistics = () => {
+
+  const axiosSecure=useAxiosSecure();
+
+  const{data:statData={},isLoading,refetch}=useQuery({
+    queryKey:["admin-stats"],
+    queryFn:async()=>{
+      const{data}=await axiosSecure.get("/admin-stats")
+      return data;
+    }
+  })
+  const{totalUsers,totalPlants}=statData;
+
+  if(isLoading){
+    return <LoadingSpinner></LoadingSpinner>
+  }
+
   return (
     <div>
       <div className="mt-12">
@@ -52,7 +71,7 @@ const AdminStatistics = () => {
                 Total Plants
               </p>
               <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
-                120
+                {totalPlants}
               </h4>
             </div>
           </div>
@@ -68,7 +87,7 @@ const AdminStatistics = () => {
                 Total User
               </p>
               <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900">
-                10
+                {totalUsers}
               </h4>
             </div>
           </div>
